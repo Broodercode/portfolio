783cf2555c7c27ad7375f6b7f8b9a7fe146e2176
broodercode <c_schiebelbein@hotmail.com>
2024-01-29 13:31:21 -0700

    design framing

--- src/views/DesignView.vue
+++ src/views/DesignView.vue
@@ -1,19 +1,30 @@
-
-
 <template>
     <div class="base">
         <b-container fluid class="container">
             <b-row>
-                <b-col sm="12" md="4" class="column-one" style="min-height: 150vh; transform: translateY(-25%) rotate(22.5deg); z-index: -1; overflow: hidden;">
+                <b-col sm="12" md="4" class="column-one">
+                    <div class="focus">
+                    <h2 class="column-title">Pizza Website</h2>
                     <div class="shader">
                     <div class="pizza-image"></div>
                 </div>
+                </div>
                 </b-col>
-                <b-col sm="12" md="4" class="column-two" style="min-height: 150vh; transform: translateY(-25%) rotate(22.5deg); z-index: -1;">
-                    <!-- Content for column two -->
+                <b-col sm="12" md="4" class="column-two" >
+                    <div class="focus">
+                    <h2 class="column-title">Furniture Store Design</h2>
+                    <div class="shader">
+                    <div class="pizza-image"></div>
+                </div>
+                </div>
                 </b-col>
-                <b-col sm="12" md="4" class="column-three" style="min-height: 150vh; transform: translateY(-25%) rotate(22.5deg); z-index: -1;">
-                    <!-- Content for column three -->
+                <b-col sm="12" md="4" class="column-three">
+                         <div class="focus">
+                    <h2 class="column-title">Art</h2>
+                    <div class="shader">
+                    <div class="pizza-image"></div>
+                </div>
+                </div>
                 </b-col>
             </b-row>
         </b-container>
@@ -29,26 +40,50 @@ export default {
 <style lang="scss" scoped>
 .base {
     background-color: #0f0f0f; /* Light Gray */
-    z-index: -2;
+    z-index: 0;
     position: relative;
     display: block;
     width: 100vw;
     height: 100vh;
     overflow: hidden;
 }
+.focus {
+    position: relative;
+    height: 100%;
+    width: 100% ;
+    &:hover {
+        z-index: 1;
+        transform: scale(1.1);
+        .shader {
+            opacity: 1;
+            background-color: transparent
+        }
+    }
+    transition: ease-in-out 0.3s;
+}
+.column-one, .column-two, .column-three {
+    min-height: 150vh; 
+    transform: translateY(-25%) rotate(22.5deg); 
+
+    &:hover {
+        transform: translateY(-25%) rotate(22.5deg) scale(1.1);
+        z-index: 1;
+    }
+}
 
 .column-one {
     overflow: hidden;
+    transition: transform 0.3s ease-in-out;
 }
 
 .column-two {
-    background-color: #00ff00; /* Green */
     overflow: hidden;
+    transition: transform 0.3s ease-in-out;
 }
 
 .column-three {
-    background-color: #0000ff; /* Blue */
     overflow: hidden;
+    transition: transform 0.3s ease-in-out;
 }
 
 .pizza-image {
@@ -56,12 +91,24 @@ export default {
     width: 100%;
     height: 100%;
     background-position: center;
- 
+
 }
 .shader {
     background-color: #010101;
     opacity: 0.4;
     height: 100%;
     width: 100%;
+    overflow: hidden;
+
+}
+.column-title {
+    position: relative;
+    top: 50%;
+    transform: translateY(-50%);
+    color: white;
+    text-align: center;
+    transform: rotate(-22.5deg);
+    z-index: 1;
+    font-weight: bolder;
 }
 </style>
